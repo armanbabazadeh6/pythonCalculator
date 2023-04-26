2c8c11ac512005e40897ecc54cf12ad12e531ec8
Arman Babazadeh <armanbabazadeh61@gmail.com>
2023-04-25 23:26:20 -0400

    Added small update

--- calculator.py
+++ calculator.py
@@ -15,35 +15,36 @@ def div(a,b):
     answer = a / b
     print(str(a) + " / " + str(b) + " = " + str(answer))
 
+while True:
+    print("A. Addition")
+    print("B. Subtraction")
+    print("C. Multiplication")
+    print("D. Division")
+    print("E. Exit")
 
-print("A. Addition")
-print("B. Subtraction")
-print("C. Multiplication")
-print("D. Division")
 
+    choice = input("Enter your choice: ")
 
-choice = input("Enter your choice: ")
-
-if(choice == "a" or choice == "A"):
-    print("Addition")
-    a = int(input("Input first number: "))
-    b = int(input("Input seccond number: "))
-    add(a, b)
-elif choice == "b" or choice == "B":
-    print("Subtraction")
-    a = int(input("Input first number: "))
-    b = int(input("Input seccond number: "))
-    sub(a, b)
-elif choice == "c" or choice == "C":
-    print("Multiplication")
-    a = int(input("Input first number: "))
-    b = int(input("Input seccond number: "))
-    mul(a, b)
-elif choice == "d" or choice == "D":
-    print("Division")
-    a = int(input("Input first number: "))
-    b = int(input("Input seccond number: "))
-    div(a, b)
-elif choice == "e" or choice == "E":
-    print("Exited Program")
-    quit()
+    if(choice == "a" or choice == "A"):
+        print("Addition")
+        a = int(input("Input first number: "))
+        b = int(input("Input seccond number: "))
+        add(a, b)
+    elif choice == "b" or choice == "B":
+        print("Subtraction")
+        a = int(input("Input first number: "))
+        b = int(input("Input seccond number: "))
+        sub(a, b)
+    elif choice == "c" or choice == "C":
+        print("Multiplication")
+        a = int(input("Input first number: "))
+        b = int(input("Input seccond number: "))
+        mul(a, b)
+    elif choice == "d" or choice == "D":
+        print("Division")
+        a = int(input("Input first number: "))
+        b = int(input("Input seccond number: "))
+        div(a, b)
+    elif choice == "e" or choice == "E":
+        print("Exited Program")
+        quit()
